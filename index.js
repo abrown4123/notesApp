1ba086ad907d93c98a73858b993a5584eba69d2e
Anthony Brown <abrown4123@gmail.com>
2022-04-16 23:05:37 -0500

    add server side sanitation

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require("dotenv").config()
 const cors = require("cors")
 const express = require("express")
 const app = express()
+const Note = require('./models/note')
+const {unknownEndpoint, errorHandler} = require("./middleware/error");
+
 
 app.use(express.static('build'))
 app.use(cors())
@@ -42,35 +46,39 @@ app.get("/api/notes", (request, response) => {
   response.json(notes)
 })
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({
-      error: 'content missing'
-    })
-  }
-
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId()
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+  .catch(error => next(error))
 })
 
 app.get("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
+  Note.findById(request.params.id).then(note => {
     response.json(note)
-  } else {
-    response.status(404).end()
-  }
+  })
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const {content, important} = request.body
+
+  Note.findByIdAndUpdate(
+    request.params.id,
+    {content, important},
+    {new: true, runValidators: true, context: 'query'}
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -78,6 +86,9 @@ app.delete("/api/notes/:id", (request, response) => {
   notes = notes.filter(note => note.id !== id)
   response.status(204).end()
 })
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
