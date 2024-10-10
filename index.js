6d6fa8ae4b108471cbce87d782273405c6540f78
Anthony Brown <abrown4123@gmail.com>
2024-10-09 19:22:41 -0500

    Update the delete endpoint to connect to the DB

--- index.js
+++ index.js
@@ -84,9 +84,11 @@ app.put('/api/notes/:id', (request, response, next) => {
 })
 
 app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
